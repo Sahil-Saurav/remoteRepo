5bcac94a0842e53bce64aa0b279527883d094b74
Sahil Saurav <shaheel7112002@gmail.com>
2025-05-28 15:27:43 +0530

    Deployed on Vercel

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const app = express();
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); // Correct path to the views directory
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Home route
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page' });
+    res.render('index', { title: 'Home Page' }); // Don't include .ejs extension
 });
 
 // Start the server
